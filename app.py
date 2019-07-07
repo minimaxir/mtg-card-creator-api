f2d9f42d3b38a20e3a1e1212894d9144c147b810
Max Woolf <max@minimaxir.com>
2019-07-07 10:58:50 -0700

    turn off debug

--- app.py
+++ app.py
@@ -50,5 +50,4 @@ def homepage():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)),
-            debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
